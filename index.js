7c7104651dfc99441ef3779440d08809e4a4db85
neeraj nath <neeraj.nath987@gmail.com>
2023-10-10 18:55:23 +0530

    Implement Mongo Database to store the Todo items

--- index.js
+++ index.js
@@ -1,50 +1,80 @@
 import express from "express";
 import bodyParser from "body-parser" ;
+import mongoose, { Schema } from "mongoose";
 const app = express();
 const port = 3000;
-var listOfPersonalTasks = [];
-var listOfWorkTasks = [];
+// var listOfPersonalTasks = [];
+// var listOfWorkTasks = [];
+mongoose.connect('mongodb://0.0.0.0:27017/me-ToDo');
+
+const db = mongoose.connection;
+
+db.on('error',()=>{
+    console.error.bind('Error Connecting to Database');
+});
+
+db.once('open',()=>{
+    console.log('Successfully connected to Database..');
+});
+
+const ToDoSchema = new Schema({
+    name : {
+        type : String,
+        required : true
+    }
+});
+
+const ToDoList = mongoose.model("ToDoList",ToDoSchema);
+const WorkToDoList = mongoose.model("WorkToDoList", ToDoSchema);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // Static Middleware
 app.use(express.static('public'));
 
-app.get('/', (req,res) => {
+app.get('/', async (req,res) => {
     try{
+        var items = await ToDoList.find({}).exec();
         res.render('index.ejs', {
             listTitle: new Date().toDateString(),
-            task : listOfPersonalTasks,
+            task : items,
             action : '/submit'
-        });
+        })
+        
     }catch(error){
-        res.send('404!!');
+        console.error("The error is :-",error.message);
+        res.send('<h1>404!!</h1>');
     }
 });
-app.get('/work', (req,res) => {
+
+app.get('/work', async(req,res) => {
     try{
+        var workItems = await WorkToDoList.find({}).exec();
         res.render('index.ejs', {
-            listTitle: 'Work To-Dos',
-            task : listOfWorkTasks,
+            listTitle: 'Work-ToDos',
+            task : workItems,
             action : '/submit-work'
-        });
+        })
     }catch(error){
-        res.send('404!!');
+        console.error("The error is :-",error.message);
+        res.send('<h1>404!!</h1>');
     }
 });
-app.post('/submit',(req,res) => {
+app.post('/submit', async(req,res) => {
     try{ 
-        const task = req.body.task;
-        listOfPersonalTasks.push(task);
+        await ToDoList.create({ 
+            name : req.body.task,
+        });
         res.redirect('/');
     }catch(error){
         console.error(error.message);
         res.send('<h1>Some Error Occured!!</h1>');
     }
 });
-app.post('/submit-work',(req,res) => {
+app.post('/submit-work', async(req,res) => {
     try{ 
-        const task = req.body.task;
-        listOfWorkTasks.push(task);
+        await WorkToDoList.create({ 
+            name : req.body.task,
+        });
         res.redirect('/work');
     }catch(error){
         console.error(error.message);
