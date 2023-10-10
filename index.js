b6b0bc46a57b659f1d078875e189e50d6b867735
neeraj nath <neeraj.nath987@gmail.com>
2023-10-10 10:03:23 +0530

    add styles and checked functionality for TODO

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser" ;
 const app = express();
 const port = 3000;
 var listOfTasks = ["get a dog", "buy a car"];
+
+app.use(bodyParser.urlencoded({ extended: false }));
 // Static Middleware
 app.use(express.static('public'));
 
@@ -12,6 +15,16 @@ app.get('/', (req,res) => {
         });
     }catch(error){
         res.send('404!!');
+    }
+})
+app.post('/submit',(req,res) => {
+    try{ 
+        const task = req.body.task;
+        listOfTasks.push(task);
+        res.redirect('/');
+    }catch(error){
+        console.error(error.message);
+        res.send('<h1>Some Error Occured!!</h1>');
     }
 })
 app.listen( port , ()=>{
