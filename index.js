31974e880b5566dec955ebd827ee61090fdf62d2
neeraj nath <neeraj.nath987@gmail.com>
2023-10-10 11:00:39 +0530

    Add Two profile capability - Work and Personal Todos

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 import bodyParser from "body-parser" ;
 const app = express();
 const port = 3000;
-var listOfTasks = ["get a dog", "buy a car"];
+var listOfPersonalTasks = ["get a dog", "buy a car"];
+var listOfWorkTasks = ['Crack a better salary job'];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // Static Middleware
@@ -11,16 +12,39 @@ app.use(express.static('public'));
 app.get('/', (req,res) => {
     try{
         res.render('index.ejs', {
-            task : listOfTasks
+            listTitle: new Date().toDateString(),
+            task : listOfPersonalTasks,
+            action : '/submit'
         });
     }catch(error){
         res.send('404!!');
     }
-})
+});
+app.get('/work', (req,res) => {
+    try{
+        res.render('index.ejs', {
+            listTitle: 'Work To-Dos',
+            task : listOfWorkTasks,
+            action : '/submit-work'
+        });
+    }catch(error){
+        res.send('404!!');
+    }
+});
 app.post('/submit',(req,res) => {
     try{ 
         const task = req.body.task;
-        listOfTasks.push(task);
+        listOfPersonalTasks.push(task);
+        res.redirect('/');
+    }catch(error){
+        console.error(error.message);
+        res.send('<h1>Some Error Occured!!</h1>');
+    }
+});
+app.post('/submit-work',(req,res) => {
+    try{ 
+        const task = req.body.task;
+        listOfWorkTasks.push(task);
         res.redirect('/');
     }catch(error){
         console.error(error.message);
